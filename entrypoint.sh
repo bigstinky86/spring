c7adcfad29b3a5274b8fcb46115ad7c50f199acc
Dmitriy SKopintsev <dmitriy_skopintsev@epam.com>
2020-06-12 23:04:20 +0600

    sonarscan added

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,15 +7,15 @@ if [[ -z "${SONAR_TOKEN}" ]]; then
   exit 1
 fi
 
-if [[ -f "pom.xml" ]]; then
-  echo "Maven project detected. You should run the goal 'org.sonarsource.scanner.maven:sonar' during build rather than using this GitHub Action."
-  exit 1
-fi
-
-if [[ -f "build.gradle" ]]; then
-  echo "Gradle project detected. You should use the SonarQube plugin for Gradle during build rather than using this GitHub Action."
-  exit 1
-fi
+#if [[ -f "pom.xml" ]]; then
+#  echo "Maven project detected. You should run the goal 'org.sonarsource.scanner.maven:sonar' during build rather than using this GitHub Action."
+#  exit 1
+#fi
+#
+#if [[ -f "build.gradle" ]]; then
+#  echo "Gradle project detected. You should use the SonarQube plugin for Gradle during build rather than using this GitHub Action."
+#  exit 1
+#fi
 
 if [[ -z "${SONARCLOUD_URL}" ]]; then
   SONARCLOUD_URL="https://sonarcloud.io"
